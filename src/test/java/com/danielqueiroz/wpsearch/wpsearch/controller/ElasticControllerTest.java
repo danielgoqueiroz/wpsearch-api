d902c57ffe3b77b579f6f3d060eb50549db21bc3
Daniel Gomes Queiroz <contato@danielqueiroz.com>
2021-11-12 18:09:42 -0300

    Ajuste de teste

--- src/test/java/com/danielqueiroz/wpsearch/wpsearch/controller/ElasticControllerTest.java
+++ src/test/java/com/danielqueiroz/wpsearch/wpsearch/controller/ElasticControllerTest.java
@@ -4,12 +4,15 @@ import static com.danielqueiroz.wpsearch.wpsearch.service.ElasticClientService.g
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 
 import org.elasticsearch.action.DocWriteResponse.Result;
 import org.elasticsearch.action.index.IndexRequest;
@@ -17,22 +20,73 @@ import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
-import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
 
 import model.Person;
+import model.Post;
 
 public class ElasticControllerTest {
 
     @BeforeAll
     public static void setUp() {
+    }
+
+    @Test
+    public void salvarPublicações() throws IOException {
+        File baseFolder = new File("D:/Desenvolvimento/MachineLearning/Dados Ndmais/2021");
+        Files.walk(baseFolder.toPath()).filter(Files::isRegularFile).forEach(filePath -> {
+            try {
+                JSONObject json = JSON.parseObject(new String(Files.readAllBytes(filePath)), JSONObject.class);
+                // String json = new String(Files.readAllBytes(filePath));
+                IndexRequest request = new IndexRequest("publicacao");
+                request.source(json);
+                IndexResponse response = getClient().index(request, RequestOptions.DEFAULT);
+                assertEquals(Result.CREATED, response.getResult());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    @Test
+    public void buscarPublicacoes() throws IOException {
+
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("publicacao");
+        searchRequest.source(SearchSourceBuilder.searchSource().size(10));
+        SearchResponse sResponse = getClient().search(searchRequest, RequestOptions.DEFAULT);
+        SearchHit[] searchHits = sResponse.getHits().getHits();
+        List<SearchHit> postsES = Arrays.asList(searchHits);
+        // Post.parserElasticToObject(JSON.parseObject(item.getSourceAsString(),
+        // JSONObject.class))
+
+        postsES.stream().map(hit -> {
+            JSONObject json = new JSONObject(hit.getSourceAsMap());
+
+            System.out.println(json);
+            Post post = new Post();
+            String id = json.getString("id");
+            String title = json.getString("title");
+            String content = json.getString("content").toString();
+            String author = json.getJSONArray("_embedded").getJSONObject(0).getJSONObject("author").getString("name");
+
+            String date = json.getString("date");
+
+            post.setId(Integer.parseInt(id));
+            post.setTitle(title);
+            post.setContent(content);
+            post.setAuthor(author);
+            post.setDate(date);
+            return post;
+        }).collect(Collectors.toList());
+        System.out.println(postsES);
+
+        // assertTrue(resultsList.size() > 0);
     }
 
     @Test
---
+++ src/test/java/model/Post.java
@@ -0,0 +1,125 @@
+package model;
+
+import java.util.List;
+
+import com.alibaba.fastjson.JSONObject;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@EntityScan
+public class Post {
+
+    private Integer id;
+    private String date;
+    private String title;
+    private String link;
+    private String type;
+    private String content;
+    private String excerpt;
+    private String author;
+    private List<String> metas;
+    private List<String> categories;
+    private List<String> tags;
+
+    public Post() {
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public List<String> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<String> categories) {
+        this.categories = categories;
+    }
+
+    public List<String> getMetas() {
+        return metas;
+    }
+
+    public void setMetas(List<String> metas) {
+        this.metas = metas;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getExcerpt() {
+        return excerpt;
+    }
+
+    public void setExcerpt(String excerpt) {
+        this.excerpt = excerpt;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public static Post parserElasticToObject(JSONObject json) {
+        Post post = new Post();
+        post.setId(Integer.parseInt(json.getString("id").toString()));
+        post.setTitle(json.getString("title").toString());
+        post.setContent(json.getString("content").toString());
+        post.setAuthor(json.getString("author").toString());
+        post.setDate(json.getString("date").toString());
+        return post;
+    }
+
+}
